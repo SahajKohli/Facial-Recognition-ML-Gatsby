65ddb07a43c672aabc413a02b6377233b4c4f145
Sahaj Kohli <sahaj@sahajs-mbp.lan>
2020-04-30 01:05:23 -0400

    Can find out if the person is Chad or Will

--- facial-recog/src/pages/index.js
+++ facial-recog/src/pages/index.js
@@ -1,21 +1,100 @@
 import React from "react"
-import { Link } from "gatsby"
+import * as faceapi from 'face-api.js'
+import Dropzone from 'react-dropzone';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
 
-export default IndexPage
+export default class Index extends React.Component {
+
+  state = {
+    loading: true,
+    isWill: false,
+    isChad: false,
+    file: null,
+  }
+
+  async componentDidMount(){
+    console.log("hello");
+    //console.log(faceapi.fetchImage('./static/willF.jpg'));
+    await Promise.all([
+      await faceapi.loadSsdMobilenetv1Model('./face_model/'),
+      await faceapi.loadFaceLandmarkModel('./face_model'),
+      await faceapi.loadFaceRecognitionModel('./face_model'),
+    ]);
+
+    this.setState({
+      loading: false,
+    });
+
+  }
+
+  async checkFace (file){
+
+    if(!file){
+    return;
+  }
+    const will = await faceapi.fetchImage('./willF.jpg');
+    const chad = await faceapi.fetchImage('./chadS.jpg');
+    const uploadImage = await faceapi.fetchImage(file);
+
+    const willDescriptor = await faceapi.allFacesSsdMobilenetv1(will);
+    const charDescriptor = await faceapi.allFacesSsdMobilenetv1(chad);
+    const uploadDescriptor = await faceapi.allFacesSsdMobilenetv1(uploadImage)
+
+    const willDistance = faceapi.euclideanDistance(willDescriptor[0].descriptor, uploadDescriptor[0].descriptor);
+    const chadDistance = faceapi.euclideanDistance(charDescriptor[0].descriptor, uploadDescriptor[0].descriptor);
+
+
+    console.log("hello");
+
+    this.setState({
+      loading: false,
+      isWill : willDistance < 0.6,
+      isChad : chadDistance < 0.6,
+      file:file,
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          this.state.loading ? (
+            <p>Loading......</p>
+          ) : (
+            <Dropzone onDrop={files =>
+            {
+              console.log(files);
+              const file = files[0];
+              const reader = new FileReader();
+
+              reader.onload = () => {
+                const fileAsBinaryString = reader.result;
+
+                this.checkFace(fileAsBinaryString);
+              };
+              reader.readAsDataURL(file);
+            }}>
+              {({getRootProps, getInputProps}) => (
+                <div className="container">
+                  <div
+                    {...getRootProps({
+                      className: 'dropzone',
+                      onDrop: event => event.stopPropagation()
+                    })}
+                  >
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or click to select files </p>
+                  </div>
+                </div>
+              )}
+            </Dropzone>
+          )
+        }
+        {this.state.file && <img src ={this.state.file} alt="uploaded image"/>}
+        <h2>This is {this.state.isWill ? 'Will' : '' } {this.state.isChad ? 'Chad': ''}{!this.state.isWill  && !this.state.isChad ? 'not either':''} </h2>
+
+      </div>
+    )
+  }
+}
